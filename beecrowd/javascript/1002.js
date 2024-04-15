44f3b32a294f6dc2e7fd24b7ea6ea471110d5e8f
Gustavo Borges <157196538+gsnoopy@users.noreply.github.com>
2024-04-14 23:44:27 -0300

    Add 1003, remove js, dstore in gitignore

--- beecrowd/javascript/1002.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-var raio = parseFloat(lines.shift());
-const n = 3.14159
-let area = (n * (raio**2)).toFixed(4)
-
-console.log('A=' + area);
